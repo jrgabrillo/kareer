475180efa3c00086064e054fb6a9b6341d101ff0
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2017-07-26 17:03:14 +0800

    update

--- kareer/assets/harmony/mobile.php
+++ kareer/assets/harmony/mobile.php
@@ -2,17 +2,31 @@
 session_start();
 include("Functions.php");
 $Functions = new DatabaseClasses;
+    
+    if (isset($_GET['do-logIn'])){
+       $data = $_POST["data"];
+        
+        $email = $data[0]['value'];
+        $password = sha1($data[1]['value'] );
 
-    //[CED] signup,saving data to database
+        $query = $Functions->PDO("SELECT COUNT(*) FROM tbl_applicant  WHERE email = '{$email}' AND password = '{$password}'");
+        if($query[0][0]==0){
+            echo 0;
+        }
+        else if($query[0][0]==1){
+            $_SESSION["kareer7836"] = [$data[0]['value'],$password,'applicant'];
+            echo 1;
+        }
+    }
+
     if (isset($_GET['do-signUp'])){
         $data = $_POST['data'];
-        // print_r($data);
-        $id = $Functions->PDO('tbl_applicant','id');
-        $name = $data[0]['value'];
+        $id = $Functions->PDO_IDGenerator('tbl_applicant','id');
+        $name = $Functions->escape($data[0]['value']);
         $email = $data[1]['value'];
-        $password = sha1($data[2]['value'] );
+        $password = sha1($data[2]['value']);
 
-        $query = $Functions->PDO("INSERT INTO tbl_applicant(id,lname,fname,mname,address,contactno,image,description,resume,email,password,gender) VALUES('{$id}','{$name}','','','','','','','','{$email}','{$password}','')");;
+        $query = $Functions->PDO("INSERT INTO tbl_applicant(id,description,resume,email,password) VALUES('{$id}','','','{$email}','{$password}')");
         if($query->execute()){
             echo 1;
         }
@@ -24,7 +38,6 @@ $Functions = new DatabaseClasses;
 
     if (isset($_GET['do-academic-info'])){
         $data = $_POST['data'];
-        // print_r($data);
         $id = $Functions->PDO_IDGenerator('tbl_acadinfo','id');
         $level = $data[0]['value'];
         $sattended = $data[1]['value'];
@@ -34,7 +47,7 @@ $Functions = new DatabaseClasses;
         $ygraduated = $data[5]['value'];
         $aid = $data[6]['value'];
 
-        $query = $Functions->PDO("INSERT INTO tbl_acadinfo(id,level,schoolattended,degree,periodofattendance,highestlevel,yeargraduated,applicant_id)VALUES('{$id}','{$level}','{$sattended}','{$degree}','{$pattended}','{$hlevel}','{$ygraduated}','{$aid}')");;
+        $query = $Functions->PDO("INSERT INTO tbl_acadinfo(id,level,schoolattended,degree,periodofattendance,highestlevel,yeargraduated,applicant_id) VALUES('{$id}','{$level}','{$sattended}','{$degree}','{$pattended}','{$hlevel}','{$ygraduated}','{$aid}')");;
         if($query->execute()){
             echo 1;
         }
@@ -57,7 +70,7 @@ $Functions = new DatabaseClasses;
         $gservice = $data[6]['value'];
         $date = $data[6]['value'];
 
-        $query = $Functions->PDO("INSERT INTO tbl_career(id,applicant_id,inclusive_dates,position_title,agency,monthly_salary,appointment_status,govt_service,date)VALUES('{$id}','{$aid}','{$idates}','{$ptitle}','{$agency}','{$msalary}','{$astatus}','{$gservice}','$date')");;
+        $query = $Functions->PDO("INSERT INTO tbl_career(id,applicant_id,inclusive_dates,position_title,agency,monthly_salary,appointment_status,govt_service,date) VALUES('{$id}','{$aid}','{$idates}','{$ptitle}','{$agency}','{$msalary}','{$astatus}','{$gservice}','$date')");;
         if($query->execute()){
             echo 1;
         }
@@ -67,20 +80,4 @@ $Functions = new DatabaseClasses;
         }
     }
 
-    //RELKINS
-    if (isset($_GET['do-logIn'])){
-       $data = $_POST["data"];
-        
-        $email = $data[0]['value'];
-        $password = sha1($data[1]['value'] );
-
-        $query = $Functions->PDO("SELECT COUNT(*) FROM tbl_applicant  WHERE email = '{$email}' AND password = '{$password}'");
-        if($query[0][0]==0){
-            echo 0;
-        }
-        else if($query[0][0]==1){
-            $_SESSION["kareer7836"] = [$data[0]['value'],$password,'applicant'];
-            echo 1;
-        }
-    }
 ?> 
