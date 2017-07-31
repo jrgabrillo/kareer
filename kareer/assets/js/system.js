c9e31d403d388f0420dd33a8b357411a5971e5b2
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2017-07-31 21:43:29 +0800

    update

--- kareer/assets/js/system.js
+++ kareer/assets/js/system.js
@@ -69,10 +69,12 @@ var system = function(){
 		    return objects;
 		},
 		froala:function(element){
-			$(element).froalaEditor({
-				toolbarButtons:['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'spellChecker', 'help', 'html', '|', 'undo', 'redo'],
-				quickInsertButtons: ['table', 'ol', 'ul', 'hr']
-			});
+		    $(function() {
+				$(element).froalaEditor({
+					toolbarButtons:['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'spellChecker', 'help', 'html', '|', 'undo', 'redo'],
+					quickInsertButtons: ['table', 'ol', 'ul', 'hr']
+				});
+		    });
 		},
 		forceLogout:function(_function){ //300000
 			$(document).idle({
